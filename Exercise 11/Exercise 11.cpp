aab50e084956c5296f443ecf0b4c6f794f35aa00
Виталий Медведев <MedVital@mail.ru>
2021-08-26 17:07:55 +0300

    11 упражнение

---
+++ Exercise 11/Exercise 11.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <typeinfo>
+
+using std::string; using std::cout; using std::cin; using std::getline; using std::endl;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    return 0;
+}
